5a8f23b13089ba96dd8a8e73aafa2c4daf54701f
jalmir <jalmirfsjr@gmail.com>
2024-02-29 09:05:55 -0300

    feat: install harpoon plugin

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,6 +9,10 @@ M.setup = function()
   vim.keymap.set('n', '<leader>ff', '<CMD>Telescope find_files<CR>', opts)
   vim.keymap.set('n', '<leader>fd', '<CMD>Telescope treesitter<CR>', opts)
 
+  -- Harpoon
+  vim.keymap.set("n", "<leader>a", function() require("harpoon"):list():append() end)
+  vim.keymap.set("n", "<C-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end)
+
   -- LSP
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
 end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -112,5 +112,13 @@ return {
       require("which-key").setup()
     end,
   },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup()
+    end
+  },
   { "folke/neodev.nvim" },
 }
