643e0d5f800e7ed71d1d10de2416d22b434c0089
jalmir <jalmirfsjr@gmail.com>
2024-03-07 21:48:41 -0300

    feat: remove tab key from autocomplete

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,57 +3,38 @@ local cmp_action = require("lsp-zero").cmp_action()
 local luasnip = require("luasnip")
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  formatting = {
-    fields = { "abbr", "kind", "menu" },
-    format = require("lspkind").cmp_format({
-      maxwidth = 50,
-      ellipsis_char = "...",
-      mode = "symbol_text",
-    }),
-  },
-  mapping = {
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<C-k>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-j>"] = cmp.mapping.scroll_docs(4),
-    ["<C-c>"] = cmp.mapping.abort(),
-    ["<C-n>"] = { i = cmp.mapping.complete() },
-    ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-    ["<C-b>"] = cmp_action.luasnip_jump_backward(),
-  },
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "nvim_lua" },
-    { name = "luasnip" },
-    {
-      name = "path",
-      option = { trailing_slash = true }
-    },
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	formatting = {
+		fields = { "abbr", "kind", "menu" },
+		format = require("lspkind").cmp_format({
+			maxwidth = 50,
+			ellipsis_char = "...",
+			mode = "symbol_text",
+		}),
+	},
+	mapping = {
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<C-k>"] = cmp.mapping.scroll_docs(-4),
+		["<C-j>"] = cmp.mapping.scroll_docs(4),
+		["<C-c>"] = cmp.mapping.abort(),
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip" },
+		{
+			name = "path",
+			option = { trailing_slash = true }
+		},
+	},
 })
